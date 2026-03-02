a46ea55f8394285e2d171ef94c412aee23acdc88
Talha Muhammad Suleiman <msgonfealgorpa@gmail.com>
2026-03-02 20:58:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  basePath: "/universal-ai-life-os",
+
   reactStrictMode: true,
+
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'api.dicebear.com']
   }
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
