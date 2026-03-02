8aa574f07cd7ce985c27323348a8eb12685ad3c0
Talha Muhammad Suleiman <msgonfealgorpa@gmail.com>
2026-03-02 21:50:29 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
@@ -26,4 +27,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
